f3db175b373dca22b7ea3e7306790585686f29af
gunasekaran-code <gunasekaran.code@gmail.com>
2026-06-09 00:18:29 +0530

    Add: open-next.config.ts

---
+++ open-next.config.ts
@@ -0,0 +1,5 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+  // You can leave this empty to use the standard Cloudflare defaults
+});
